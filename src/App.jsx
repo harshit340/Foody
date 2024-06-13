c2666e607682ba12b01bc82942cfc35a255b1c02
Aryanmittal23 <mittalaryan932@gmail.com>
2024-06-14 02:16:29 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css'
 import Navbar from "./components/navabar/Navbar";
 import { Routes,Route} from 'react-router-dom';
 import Cart from "./pages/Cart/Cart";
 import Home from './pages/Home/Home';
 import PlaceOrder from './pages/PlaceOrder/PlaceOrder';
-
+import Footer from './components/Footer/Footer';
+import LoginPopup from './components/LoginPopup/LoginPopup';
+//import Header from './components/Header/Header';
 function App() {
+    const [showLogin,setShowLogin]=useState(false)
 
   return (
-    <div className="cpp">
-      
-  <Navbar />
-  <Routes>
-    <Route path='/' element={<Home/>} />
-    <Route path='/cart' element={<Cart/>} />
-    <Route path='/order' element={<PlaceOrder/>}/>
-  </Routes>
-  </div>
+  <>
+  {showLogin?<LoginPopup setShowLogin={setShowLogin}/>:<></>}
+    <div className="app">
+      <Navbar setShowLogin={setShowLogin} />
+      <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='/cart' element={<Cart/>} />
+        <Route path='/order' element={<PlaceOrder/>}/>
+      </Routes>
+    </div>
+    <Footer/>
+  </>
   )
 }
 
